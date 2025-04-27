7ce757034fc8273a982e0ff63975cc029e69959d
ellis-chang <echang41@gmail.com>
2025-04-27 18:36:06 -0400

    implement JWT authentication

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,22 @@
 import axios from 'axios';
+import { getToken } from './authService';
 
 const API = axios.create({
   baseURL: 'http://127.0.0.1:5000',  // Flask backend URL
   withCredentials: true,  // For handling cookies if needed later
 });
+
+API.interceptors.request.use(
+  (config) => {
+    const token = getToken();
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 export default API;
