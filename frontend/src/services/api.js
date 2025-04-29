3c5784d16ece9cc96da25dbd1265601685bb3077
ellis-chang <echang41@gmail.com>
2025-04-29 12:18:06 -0400

    trying to get deployment working

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL|| 'http://127.0.0.1:5000' || 'https://ai-study-coach.onrender.com',  // Flask backend URL
+  baseURL: 'https://ai-study-coach.onrender.com',  // Flask backend URL
   withCredentials: true,  // For handling cookies if needed later
   headers: {
     'Content-Type': 'application/json'  
